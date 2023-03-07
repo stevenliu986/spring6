2f12fc9842206ceec4cc6d46daa67484f4eea932
StevenLiu <stevenliu986@gmail.com>
2023-03-08 08:43:16 +1100

    创建ioc配置模块

---
+++ spring6-ioc-xml/src/main/java/com/learning/spring6/iocxml/User.java
@@ -0,0 +1,18 @@
+package com.learning.spring6.iocxml;
+
+/**
+ * ClassName: User
+ * Package: com.learning.spring6.iocxml
+ * Description:
+ *
+ * @Author: Steven_LIU
+ * @Create: 8/03/2023
+ */
+public class User {
+    private String name;
+    private Integer age;
+
+    public void run(){
+        System.out.println("running..........");
+    }
+}
