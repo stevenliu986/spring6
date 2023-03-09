e15b134adec6c38b5d05a668a88b5b2d329613f3
StevenLiu <stevenliu986@gmail.com>
2023-03-09 22:34:19 +1100

    scope属性配置，决定是单实例还是多实例

---
+++ spring6-ioc-xml/src/main/java/com/learning/spring6/iocxml/scope/Orders.java
@@ -0,0 +1,15 @@
+package com.learning.spring6.iocxml.scope;
+
+/**
+ * ClassName: Orders
+ * Package: com.learning.spring6.iocxml.scope
+ * Description:
+ *
+ * @Author: Steven_LIU
+ * @Create: 9/03/2023
+ */
+public class Orders {
+    public void orders(){
+        System.out.println("实例。。。。。。");
+    }
+}
